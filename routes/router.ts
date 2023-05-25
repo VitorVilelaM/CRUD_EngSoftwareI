c0a21d5e3022d882a0d7a4d4a7750f36ddbac7d1
AleAoyama <133831197+AleAoyama@users.noreply.github.com>
2023-05-24 21:38:32 -0300

    Atualização do router, com a adição do Usuário

--- routes/router.ts
+++ routes/router.ts
@@ -1,8 +1,9 @@
 import { fastify } from 'fastify'
 import { ProdutoController } from '../controllers/ProdutoController';
+import { UsuarioController } from '../controllers/UsuarioController';
 
 const app = fastify();
 
 app.register(ProdutoController)
-
+app.register(UsuarioController)
 app.listen({ port: 3000 }).then(() => { console.log('listening on port 3000') });
